003a1456bb801b29024ff35ebce01cac883703c2
David L Whitehurst <dlwhitehurst@gmail.com>
2021-09-27 14:04:04 +0000

    added docker install correctly

--- node.sh
+++ node.sh
@@ -15,7 +15,24 @@ EOF
 sudo sysctl --system
 
 # Docker
-sudp apt-get install -y docker.io
+sudo apt-get remove docker docker-engine docker.io containerd runc
+sudo apt-get update
+sudo apt-get install \
+	apt-transport-https \
+	ca-certificates \
+	curl \
+	gnupg \
+	lsb-release 
+
+# docker official key
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+
+echo \
+  "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+sudo apt-get update
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io
 
 # Prep for kubernetes install
 sudo apt-get update
