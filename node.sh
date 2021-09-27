72f4c71a6e39da0e938e26f53abb2987c321be2e
David L Whitehurst <dlwhitehurst@gmail.com>
2021-09-27 18:00:19 +0000

    simplified node.sh

--- node.sh
+++ node.sh
@@ -1,54 +1,10 @@
 #!/bin/bash
 
 # This script is to be run on all nodes
+ubuntu/allow-bridge-nf-traffic.sh
+ubuntu/install-docker-2.sh
+ubuntu/update-dns.sh
 
-sudo modprobe br_netfilter
-
-cat <<EOF | sudo tee /etc/modules-load.d/k8s.conf
-br_netfilter
-EOF
-
-cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
-net.bridge.bridge-nf-call-ip6tables = 1
-net.bridge.bridge-nf-call-iptables = 1
-EOF
-sudo sysctl --system
-
-# Docker
-sudo apt-get remove docker docker-engine docker.io containerd runc
-sudo apt-get update
-sudo apt-get install -y \
-	apt-transport-https \
-	ca-certificates \
-	curl \
-	gnupg \
-	lsb-release 
-
-# docker official key
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-
-echo \
-  "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
-  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-
-sudo apt-get update
-sudo apt-get install -y docker-ce docker-ce-cli containerd.io
-
-
-# fix docker as daemon
-sudo cat > /etc/docker/daemon.json <<EOF
-{
-  "exec-opts": ["native.cgroupdriver=systemd"],
-  "log-driver": "json-file",
-  "log-opts": {
-     "max-size": "100m"
-  },
-  "storage-driver": "overlay2"
-}
-EOF
-sudo mkdir -p /etc/systemd/system/docker.service.d
-sudo systemctl daemon-reload
-sudo systemctl restart docker
 
 # Prep for kubernetes install
 sudo apt-get update
