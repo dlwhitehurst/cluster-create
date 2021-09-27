90340260b6e4e565f2aabf67b229e028a4dc4141
David L Whitehurst <dlwhitehurst@gmail.com>
2021-09-27 13:35:37 +0000

    node.sh complete

--- node.sh
+++ node.sh
@@ -14,3 +14,22 @@ net.bridge.bridge-nf-call-iptables = 1
 EOF
 sudo sysctl --system
 
+# Docker
+sudp apt-get install -y docker.io
+
+# Prep for kubernetes install
+sudo apt-get update
+sudo apt-get install -y apt-transport-https ca-certificates curl
+
+# Google signing key 
+sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
+
+# Kubernetes Apt repo
+echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+
+# Update and install
+sudo apt-get update
+sudo apt-get install -y kubeadm kubelet kubectl
+sudo apt-mark hold kubeadm kubelet kubectl
+
+# done???
