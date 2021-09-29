a40aacede7941c1dca82da17dd9f4557d4ac1183
David L Whitehurst <dlwhitehurst@gmail.com>
2021-09-29 09:24:31 -0400

    code complete

--- ubuntu/master.sh
+++ ubuntu/master.sh
@@ -5,8 +5,6 @@ echo $master
 
 # only if
 if [ "$master" = "kubemaster" ]; then 
-	echo "Install CNI solution."
-	kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml
 	echo "Initializing the master node."
         sudo kubeadm init --pod-network-cidr 10.244.0.0/16 --apiserver-advertise-address=192.168.56.2
 else 
